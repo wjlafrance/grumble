d134fe072ffddb17a47e36428b849ff980b11d45
William LaFrance <wjlafrance@gmail.com>
2014-05-28 12:05:44 -0500

    unnecessary import

--- grumble/src/main/java/net/wjlafrance/grumble/net/MurmurThread.java
+++ grumble/src/main/java/net/wjlafrance/grumble/net/MurmurThread.java
@@ -4,7 +4,6 @@ import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.security.KeyManagementException;
